91054d5f1a11943fc73bdab46ee342e8df315357
PabloMoreno2000 <pamta2000@gmail.com>
2019-11-06 12:16:37 -0600

    Added BottomNavigationBar after clicking on log in

--- app/flutter_app/lib/HomePage.dart
+++ app/flutter_app/lib/HomePage.dart
@@ -6,8 +6,51 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int iCurrentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        //It says the index of the icon
+        currentIndex: iCurrentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+            backgroundColor: Colors.blue
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            title: Text('Reservas'),
+            backgroundColor: Colors.blue
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_bar),
+            title: Text('Antros'),
+            backgroundColor: Colors.blue
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Perfil'),
+            backgroundColor: Colors.blue
+          )
+        ],
+
+        //It is called every time an item is tapped.
+          //Index is the one that was tapped
+        onTap:(index){
+          setState(() {
+            iCurrentIndex = index;
+          });
+        }
+      ),
+    );
   }
 }
