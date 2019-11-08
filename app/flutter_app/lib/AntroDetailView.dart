e138547241596ddf3f4a634be3e6d32260516d0f
PabloMoreno2000 <pamta2000@gmail.com>
2019-11-07 18:01:49 -0600

    Created Widget to show the detail view of the antro

---
+++ app/flutter_app/lib/AntroDetailView.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AntroDetailView extends StatefulWidget {
+  @override
+  _AntroDetailViewState createState() => _AntroDetailViewState();
+}
+
+class _AntroDetailViewState extends State<AntroDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
