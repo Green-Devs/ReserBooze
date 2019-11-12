e6f1a27b9f5bf0c00ca6ad36ffe462b6bd35dfd8
PabloMoreno2000 <pamta2000@gmail.com>
2019-11-11 18:58:44 -0600

    Created Reserva class

---
+++ app/flutter_app/lib/entities/Reserva.dart
@@ -0,0 +1,23 @@
+
+
+class Reserva {
+  String antroNombre;
+  String antroHora;
+  String antroFecha;
+
+  Reserva(this.antroNombre, this.antroHora, this.antroFecha);
+
+    //getters
+  String getAntroNombre() {
+    return antroNombre;
+  }
+
+  String getAntroHora() {
+    return antroHora;
+  }
+
+  String getAntroFecha() {
+    return antroFecha;
+  }
+
+}
