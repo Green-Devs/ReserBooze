9447409d3d79c527623a10d620512b3e08e05b57
PabloMoreno2000 <pamta2000@gmail.com>
2019-11-21 22:53:59 -0600

    Added all the basic elements to create the UI in CrearCuenta

--- app/flutter_app/lib/CrearCuenta.dart
+++ app/flutter_app/lib/CrearCuenta.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 import 'GlobalVariables.dart';
 import 'entities/Cuenta.dart';
+import 'GlobalVariables.dart' as globals;
 
 class CrearCuenta extends StatefulWidget {
   @override
   _CrearCuentaState createState() => _CrearCuentaState();
 }
+
+//Creando posibles mensajes de fallos al crear una cuenta
+final String correoRegistrado = "El correo ya está registrado.";
+final String ineInvalida = "INE no válida.";
+final String contrasenaCorta = "Tu contraseña debe tener 5 o más dígitos.";
+final String correoIncorrecto = "Verifique su correo electrónico.";
+
+//Creando mensaje de éxito al crear la cuenta
+final String cuentaCreada = "Cuenta creada!";
 
 class _CrearCuentaState extends State<CrearCuenta> {
   @override
@@ -25,6 +36,7 @@ class _CrearCuentaState extends State<CrearCuenta> {
     //Cada booleana dice si se va a ocultar o no el texto cuando el usuario escriba
     final hideText = [false, false, false, false, false, true];
 
+
     //Creando una imagen para visualizar el avatar del usuario
     final logo = Hero(
       tag: 'hero',
@@ -42,7 +54,17 @@ class _CrearCuentaState extends State<CrearCuenta> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(24)
         ),
+        onPressed: () {
+          String message = validateNewAccount(controllersTextFields[3].text, controllersTextFields[4].text, controllersTextFields[2].text, controllersTextFields[5].text);
+          Toast.show(message, context, duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM, backgroundColor: Colors.white, textColor: Colors.redAccent);
 
+          if(message == cuentaCreada) {
+            //Agregando cuenta a la lista de cuentas
+            globals.cuentas.add(Cuenta(controllersTextFields[0].text, controllersTextFields[3].text, controllersTextFields[5].text, DateTime.parse(controllersTextFields[1].text), int.parse(controllersTextFields[2].text), ""));
+            //Volviendo a la pantalla anterior
+            Navigator.pop(context);
+          }
+        }
       )
     );
 
@@ -50,7 +72,6 @@ class _CrearCuentaState extends State<CrearCuenta> {
     for(int i = 0; i < controllersTextFields.length; i++){
       textFormFields.add(createTextFormField(controllersTextFields[i], hintsTextFields[i], inputTypeTextFields[i], hideText[i]));
     }
-
 
     return Scaffold();
   }
@@ -70,5 +91,32 @@ class _CrearCuentaState extends State<CrearCuenta> {
         ),
       ),
     );
+  }
+
+  //Función para validar que la cuenta dada es válida
+  //Devuelve el string de cuentaCreada si es válida, sino un mensaje a desplegar
+  String validateNewAccount(String correo, String correoConfirmacion, String INE, String contrasena) {
+    String message = cuentaCreada;
+
+    //Validando que el correo no esté registrado
+    for(Cuenta cuenta in globals.cuentas) {
+      if(cuenta.getCorreo() == correo) {
+        message = correoRegistrado;
+      }
+    }
+
+    //Validando que la INE sea válida
+    if(INE.length < 3) {
+      message = ineInvalida;
+    }
+    //Checando que la contraseña sea lo suficientemente larga
+    else if(contrasena.length < 5) {
+      message = contrasenaCorta;
+    }
+    //Checando que los correos coincidan
+    else if(correo != correoConfirmacion) {
+      message = correoIncorrecto;
+    }
+    return message;
   }
 }
